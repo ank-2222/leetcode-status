e9ea7bb1efbe394e9a6543cfe80a1c2ab78336d3
Ankit kumar <ankit.kumar.info22@gmail.com>
2025-09-11 07:48:59 +0530

    Time: 83 ms (30.17%) | Memory: 53.7 MB (9.89%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+     map<int,int> mp;
+     mp[0]=1;
+     int count=0,preSum=0;
+     for(int i =0;i<nums.size();i++){
+        preSum+=nums[i];
+      count+=mp[preSum-k];
+      mp[preSum]++;
+     }  
+return count;
+    }
+};
