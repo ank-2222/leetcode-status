bc934ff0788b13467963748305fdd36d96be70d8
Ankit kumar <ankit.kumar.info22@gmail.com>
2025-09-21 12:09:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (88.57%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
-    string calc(string s) {
-        string ans;
-        int i = 0;
-        while (i < s.length()) {
-            if (s[i] == '#') {
-                ans = ans.substr(0, ans.length()-1);
-            }else{
-
-            ans += s[i];
-            }
-            i++;
-          
-        }
-        return ans;
-    }
-
     bool backspaceCompare(string s, string t) {
-        
-         return calc(s) == calc(t); }
+
+        int i = s.size() - 1;
+        int j = t.size() - 1;
+
+        int skips = 0, skipt = 0;
+
+        while (i >= 0 || j >= 0) {
+
+            while (i >= 0) {
+                if (s[i] == '#') {
+                    skips++;
+                    i--;
+                } else if (skips > 0) {
+                    skips--;
+                    i--;
+                } else
+                    break;
+            }
+            while (j >= 0) {
+                if (t[j] == '#') {
+                    skipt++;
+                    j--;
+                } else if (skipt > 0) {
+                    skipt--;
+                    j--;
+                }else
+                    break;
+            }
+
+            if (i >= 0 && j >= 0 && s[i] != t[j])
+                return false;
+            if ((i >= 0) != (j >= 0))
+                return false; // if remaining string left is of diff size it
+                              // will return false
+
+            i--;
+            j--;
+        }
+        return true;
+    }
 };
