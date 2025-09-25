6b92d94c7a63bc51145f448b83f84b7175c559ba
Ankit kumar <ankit.kumar.info22@gmail.com>
2025-09-25 09:08:08 +0530

    Time: 15 ms (20.66%) | Memory: 23.1 MB (6.87%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -16,7 +16,7 @@ public:
 
     int minEatingSpeed(vector<int>& piles, int h) {
         int low =1;
-        int high = *max_element(piles.begin(), piles.end()); //highest ffrom the pile
+        int high = 1000000000; //highest ffrom the pile
         while(high-low>1){  //now here istead of piles, our search space will be 1 to max[piles] and whichever satisfies eating in h hour , and is min , that will be answer
             int mid = (low+high)/2;
             if(canEat(piles,mid,h)){ //if its true, this is counted and shrinked high to get more lowest value (min)
