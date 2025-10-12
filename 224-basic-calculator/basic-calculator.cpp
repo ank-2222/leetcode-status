6e5146f107c4463a6c6c2c3ee133ba989b7d693c
Ankit kumar <ankit.kumar.info22@gmail.com>
2025-10-12 08:25:43 +0530

    Time: 1 ms (68.13%) | Memory: 12.3 MB (53.66%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int calculate(string s) {
+
+        stack<int> st;
+        int ans = 0;
+        int sign = 1;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (isdigit(s[i])) {
+                int num = 0;
+                while (i < n &&
+                       isdigit(s[i])) { // here i will already be increased due
+                                        // to ++i in for loop
+                    num = num * 10 + (s[i] - '0');
+                    ++i;
+                }
+                --i; // Adjust index since outer loop will increment
+                ans += num * sign;
+            } else if (s[i] == '+') {
+                sign = 1;
+            } else if (s[i] == '-') {
+                sign = -1;
+            } else if (s[i] == '(') {
+                st.push(ans);
+                st.push(sign);
+                ans = 0;
+                sign = 1;
+
+            } else if (s[i] == ')') {
+                int sign = st.top();
+                st.pop();
+                int prev = st.top();
+                st.pop();
+                ans = prev +
+                      sign * ans; // basically this is like merging prev result
+                                  // + result from sub expression to use further
+            }
+        }
+
+        return ans;
+    }
+};
